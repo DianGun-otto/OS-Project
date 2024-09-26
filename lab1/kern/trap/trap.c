6994d6970adae5e1afae14ec1694f48bcba73fc8
zhangming <zhangming@ubuntu-22.04-64bits>
2024-09-26 23:51:17 +0800

    lab1-exercise2

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -101,30 +101,25 @@ void interrupt_handler(struct trapframe *tf) {
         case IRQ_U_TIMER:
             cprintf("User software interrupt\n");
             break;
-        case IRQ_S_TIMER:
-            // "All bits besides SSIP and USIP in the sip register are
+        // "All bits besides SSIP and USIP in the sip register are
             // read-only." -- privileged spec1.9.1, 4.1.4, p59
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
             // cprintf("Supervisor timer interrupt\n");
-             /* LAB1 EXERCISE2   YOUR CODE :  */
+             /* LAB1 EXERCISE2   2213219 张高:  */
+            clock_set_next_event();
+            if(++ticks%TICK_NUM == 0){
+            	print_ticks();
+            	num++;
+            	if(num == 10){
+            	   sbi_shutdown();
+            	}
+            }
             /*(1)设置下次时钟中断- clock_set_next_event()
              *(2)计数器（ticks）加一
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
-            //cprintf("Supervisor timer interrupt\n");
-            clock_set_next_event();
-            ticks++;
-            if(ticks==TICK_NUM) {
-                cprintf("%dticks\n",TICK_NUM);
-                num++;
-                ticks=0;
-                if(num==10) {
-                    cprintf("shutdown\n");
-                    sbi_shutdown();
-                }
-            }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
@@ -158,7 +153,7 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB1 CHALLENGE3   YOUR CODE :  */
+             /* LAB1 CHALLENGE3   2211289 张铭 :  */
             /*(1)输出指令异常类型（ Illegal instruction）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
@@ -172,7 +167,7 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
-            /* LAB1 CHALLLENGE3   YOUR CODE :  */
+            /* LAB1 CHALLLENGE3   2211289 张铭 :  */
             /*(1)输出指令异常类型（ breakpoint）
              *(2)输出异常指令地址
              *(3)
